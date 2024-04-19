11ecd32ccc221a626e28595805ae53056284247b
valtzu <valtzu@gmail.com>
2024-04-19 20:54:37 +0300

    Fix UKI patching

--- patch-uki-name.sh
+++ patch-uki-name.sh
@@ -5,6 +5,8 @@
 echo -n "Patching UKI name to comply with systemd-sysupdate... "
 
 img=${1:-$(mkosi --json summary|jq -r '.Images[0]|(.OutputDirectory+"/"+.Output+".raw")')}
+IMAGE_ID=${IMAGE_ID:-$(mkosi --json summary|jq -r '.Images[0].ImageId')}
+OUTPUT=${OUTPUT:-$(mkosi --json summary|jq -r '.Images[0].Output')}
 
 if ! [ -f "$img" ] ; then
   echo "$img does not exist"
@@ -14,7 +16,7 @@ fi
 esp=$(mktemp)
 dd status=none of=$esp if=$img $(sfdisk -J $img|jq -r '.partitiontable|("bs="+(.sectorsize|tostring)+" skip=")+(.partitions[0]|((.start|tostring)+" count="+(.size|tostring)))')
 if mdir -i $esp "EFI/Linux/$IMAGE_ID-*.efi" 2>/dev/null ; then
-  mren -i $esp "EFI/Linux/$IMAGE_ID-*.efi" "EFI/Linux/${IMAGE_ID}_${IMAGE_VERSION}.efi"
+  mren -i $esp "EFI/Linux/$IMAGE_ID-*.efi" "EFI/Linux/${OUTPUT}.efi"
   dd conv=notrunc status=none if=$esp of=$img $(sfdisk -J $img|jq -r '.partitiontable|("bs="+(.sectorsize|tostring)+" seek=")+(.partitions[0]|((.start|tostring)+" count="+(.size|tostring)))')
   echo "done"
 else
